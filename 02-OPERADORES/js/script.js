2f09e2e286d43840939983ef9d3ec1cec1dfa3f5
MatheusBrands <193531342+MATBRANDS113@users.noreply.github.com>
2025-12-15 09:15:37 -0300

    update

---
+++ 02-OPERADORES/js/script.js
@@ -0,0 +1,41 @@
+/*
+// 1-Number
+console.log(2+5)
+console.log(typeof 2)
+console.log(3.7)
+
+
+// 2- ops. Aritiméticas
+console.log(2 + 8)
+console.log(2 * 8)
+console.log(8 - 2)
+console.log(2 / 8)
+
+
+// 3- Specials numbers
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(2 * "oi")
+console.log(typeof NaN)
+
+// 4 -Strings
+console.log("um texto")
+console.log('outro texto')
+console.log(`mais um`)
+*/
+
+// 5- Simbolos especiais em strings
+console.log("testando a \n quebra de linha")
+console.log("testando a \t tab")
+
+
+// 6 - Concatenação
+console.log('oi,' + ' tudo' + ' bem?')
+
+// 7 - Templete strings
+console.log(`a soma de 2 + 2 é: ${2 + 2}`)
+
+// 8 - Booleans
+console.log(2 > 1)
+console.log(5 < 20)
+console.log(5 < 3)
