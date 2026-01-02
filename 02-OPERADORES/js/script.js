dd225510c759515d858ebb9b8b2320d1c9b239de
MatheusBrands <193531342+MATBRANDS113@users.noreply.github.com>
2026-01-02 11:40:16 -0300

    save

--- 02-OPERADORES/js/script.js
+++ 02-OPERADORES/js/script.js
@@ -23,7 +23,7 @@ console.log("um texto")
 console.log('outro texto')
 console.log(`mais um`)
 */
-
+/*
 // 5- Simbolos especiais em strings
 console.log("testando a \n quebra de linha")
 console.log("testando a \t tab")
@@ -38,4 +38,44 @@ console.log(`a soma de 2 + 2 é: ${2 + 2}`)
 // 8 - Booleans
 console.log(2 > 1)
 console.log(5 < 20)
-console.log(5 < 3)
+console.log(5 < 3) 
+*/
+
+
+
+/*
+// 9- Comparação
+console.log(5 >= 5)
+console.log(5 > 5)
+console.log(10 == 10)
+console.log(10 == 9)
+console.log(10 != 9)
+
+// 10 - identico
+
+console.log(9 == "9")
+console.log(9 === "9")
+console.log(9 != "9")
+console.log(9 !== "9")
+*/
+
+// 11- OPERADORES LOGICOS
+console.log(true && true);
+console.log(true && false);
+console.log(10 > 1 && 5 < 10);
+console.log(10 > 5 && "matheus" === 2);
+console.log(10 > 5 || "matheus" === 2);
+console.log(10 > 11 && "matheus" === 2);
+console.log(!5 > 2)
+
+// 12 - empty values
+console.log(typeof undefined, typeof null)
+console.log(undefined === null)
+console.log(undefined == null)
+
+// 13 - mudança de dados
+console.log(5 * null)
+console.log("opa" * "ola")
+console.log("10" + 1) 
+console.log("10" - 1)
+
---
+++ 03-ESTRUTURAS/js/script.js
@@ -0,0 +1,52 @@
+/*
+// 1 variaveis
+let nome = "Matheus"
+
+console.log(nome)
+
+nome = "Matheus Brands"
+
+console.log(nome)
+
+const idade = 14
+console.log(idade)
+
+console.log(typeof nome, typeof idade)
+
+// 2 - mais sobre variaveis
+// let 2teste = "inválido"
+// let @teste = "inválido"
+let a = 10, b = 20, c = 30
+console.log(a, b, c)
+let _teste = "válido"
+let $teste = "válido"
+console.log($teste, _teste)
+
+// 3 - prompt
+// const age = prompt("Digite sua idade:")
+
+// console.log(`Você tem ${age} anos`)
+
+
+
+// 4 - Alert
+alert("testando")
+
+const z = 10;
+
+alert(`O número é ${z}`);
+*/
+// 5 - Object Math
+console.log(Math.max(5, 2, 1, 10))
+
+
+console.log(Math.floor(5.34))
+
+console.log(Math.ceil(5.20))
+
+// 6 - console
+console.log("teste")
+
+console.error("erro!")
+
+console.warn("teste")
