0026eff5359416e058ab43bf9e89f7297b785c4e
MatheusBrands <193531342+MATBRANDS113@users.noreply.github.com>
2026-03-12 09:49:08 -0300

    up

--- 04-FUNCOES/script.js
+++ 04-FUNCOES/script.js
@@ -1,4 +1,5 @@
-// 1 - criando uma função
+// 1 - criando uma função]
+
 function minhaFuncao() {
     console.log("Testando")
 }
@@ -43,3 +44,156 @@ function testandoEscopo() {
 console.log(`Y fora da função é: ${y}`)
 
 testandoEscopo();
+
+// 4 - Escopo aninhado
+let m = 40
+
+function escopoAninhado() {
+    let m = 30
+
+    if(true) {
+        let m = 20
+        
+    
+    if(true) {
+        let m = 10
+        console.log(m)
+    }
+
+    console.log(m)
+}
+console.log(m)
+}
+escopoAninhado();
+
+console.log(m)
+
+// 5- arrow function
+const testeFunction = () => {
+    console.log("Está é uma arrow function")
+};
+
+testeFunction();
+
+const parOuImpar = (j) => {
+    if(j % 2 === 0) {
+        console.log("Par")
+        return
+    }
+    
+    console.log("Impar")
+    
+    
+}
+    parOuImpar(8);
+
+    parOuImpar(10);
+
+    // 6 Mais sobre arrow functions
+    const raizQuadrada = (x) => {
+        return x * x
+    }
+    console.log(raizQuadrada(9));
+
+    const raizQuadrada2 = (y) => y * y
+    console.log(raizQuadrada2(8))
+
+    console.log(raizQuadrada2(4))
+
+    // 7 - parametros opcionais
+    const multiplication = function(m, n) {
+        if(n === undefined){
+            return m * 2
+        } else{
+           return m * n
+        }
+    }
+    console.log(multiplication(5))
+    console.log(multiplication(2, 3))
+
+    const greeting = (name) => {
+        if(!name){
+            console.log("olá")
+            return
+        }
+        console.log(`Olá ${name}`)
+
+        }
+        greeting("Matheus")
+        greeting()
+
+    const customGreeting = (name, greet = "olá") => {
+        return `${greet}, ${name}`
+    }
+
+    console.log(customGreeting("Matheus",))
+
+    console.log(customGreeting("João", "Bom dia"))
+
+    const repeatText = (text, repeat = 2) => {
+        for(let i = 0; i < repeat; i++)
+            console.log(text)
+    }
+
+    repeatText("Repete 3 vezes", 3)
+
+    // 9 closure
+
+    function someFunction() {
+        let txt = "alg coisa"
+
+        function display() {
+            console.log(txt)
+        }
+        display()
+    }
+
+    someFunction();
+
+    // 10- Mais sobre closure
+    const multiplicationClosure = (n) => {
+        return (m) => {
+            return n * m
+        }
+    }
+    const c1 = multiplicationClosure(5)
+
+
+    const c2 = multiplicationClosure(10)
+
+    console.log(c1)
+    console.log(c2)
+
+    console.log(c1(5))
+    console.log(c2(10))
+
+    // 11- Recursão
+
+    const untilTen = (n, m) => {
+        if(n < 10) {
+            console.log("A função parou de funcionar")
+            
+        } else{
+            const x = n - m 
+            console.log(x)
+            untilTen(x, m)
+            }
+        }
+
+untilTen(100, 7)
+
+function factorial(x) {
+    if(x === 0) {
+        return 1
+    } else{
+        return x * factorial(x - 1)
+    }
+}
+
+const num = 5
+
+const result = factorial(num)
+
+console.log(`O resultado do Número ${num} é ${result}`)
+
+    
