9e43550f633aad9edb81e24aafeefc909830beba
MatheusBrands <193531342+MATBRANDS113@users.noreply.github.com>
2026-03-14 20:19:52 -0300

    up

---
+++ 05-ARRAYS_E_OBJETOS/js/script.js
@@ -0,0 +1,24 @@
+// 1 Arrays
+const lista = [1, 2, 3, 4, 5]
+
+console.log(lista)
+
+console.log(typeof lista)
+
+const itens = ["Matheus", true, 2, 2.314, []]
+console.log(itens)
+
+// 2 Mais sobre arrays
+const arr = ["a", "b", "c", "d"]
+
+console.log(arr[3])
+
+// 3 Propriedades
+const numbers = [5, 3, 4, 7]
+
+console.log(numbers.length);
+console.log(numbers["length"]);
+
+const myName = "Matheus Brands"
+
+console.log(myName.length)
