fe6e682c8d9f4cf05b751d9e720cfa7c21ae25aa
MatheusBrands <193531342+MATBRANDS113@users.noreply.github.com>
2026-04-13 19:21:16 -0300

    up

--- 05-ARRAYS_E_OBJETOS/js/script.js
+++ 05-ARRAYS_E_OBJETOS/js/script.js
@@ -22,3 +22,64 @@ console.log(numbers["length"]);
 const myName = "Matheus Brands"
 
 console.log(myName.length)
+
+// 4 - Metodos
+const otherNumbers = [1, 2, 3]
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers)
+
+const text = "alguma coisa"
+
+console.log(text.toUpperCase())
+
+console.log(typeof text.toUpperCase)
+
+console.log( text.indexOf("m"))
+
+
+
+// 5 - Objetos
+const person = {
+    name: "Matheus",
+    age: 22,
+    job: "Desenvolvedor de software"
+}
+
+console.log(person)
+console.log(person.age)
+console.log(person.name.length)
+
+// 6 - Criando e deletando propriedades
+const car = {
+    engine: 1.8,
+    color: "black",
+    led: "purple",
+    brand: "civic",
+    model: "Type R"
+};
+
+console.log(car);
+
+car.doors = 4;
+
+console.log(car);
+
+delete car.led;
+console.log(car);
+
+//7- Mais sobre objetos
+const obj = {
+    a: "Teste",
+    b: true,
+};
+
+console.log(obj instanceof Object);
+
+const obj2 = {
+    c: []
+
+}
+Object.assign(obj2, obj)
+console.log(obj2)
